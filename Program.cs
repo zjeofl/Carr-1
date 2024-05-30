c1fcfae2f4e3405b07b1199d0fa47d4df764e680
Henrina <henrinakengue@gmail.com>
2024-05-30 22:56:30 +0200

    Creationsous programme CalculeCarre

---
+++ Program.cs
@@ -0,0 +1,9 @@
+// See https://aka.ms/new-console-template for more information
+int CalculeCarre(int nombre)
+ {
+ return nombre*nombre;
+ }
+ //Programmeprincipal
+ Console.WriteLine(CalculeCarre(4));
+ Console.WriteLine(CalculeCarre(-3));
+ Console.WriteLine(CalculeCarre(12));
